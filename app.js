6a97891bb644c2127f96a6384ab9b59d9db25dfd
Zeztron <pateltharsh@gmail.com>
2019-01-08 12:13:19 -0500

    right arrow works and added control hover effect

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 let x = 0;
 
-document.querySelector(".left-arrow").addEventListener("click", () => {
-    // console.log("clicked");
+const rotate = () => {
     const cubes = document.querySelectorAll(".cube");
-    // transform node list into arrow
+    Array.from(cubes).forEach(cube => (cube.style.transform = `rotateY(${x}deg)`));
+}
+
+document.querySelector(".left-arrow").addEventListener("click", () => {
     x += 90;
-    Array.from(cubes).forEach(cube => cube.style.transform = `rotateY(${x}deg)`);
+    rotate();
+});
+
+document.querySelector(".left-arrow").addEventListener("mouseover", () => {
+    x += 25;
+    rotate();
+});
+
+document.querySelector(".left-arrow").addEventListener("mouseout", () => {
+    x -= 25;
+    rotate();
+});
+
+document.querySelector(".right-arrow").addEventListener("click", () => {
+    x -= 90;
+    rotate();
+});
+
+document.querySelector(".right-arrow").addEventListener("mouseover", () => {
+    x -= 25;
+    rotate();
+});
+
+document.querySelector(".right-arrow").addEventListener("mouseout", () => {
+    x += 25;
+    rotate();
 });
